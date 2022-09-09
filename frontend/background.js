7b4a1642f20019cedfa294edf6ff7557fb613e59
Sahil Prajapati <sprajapati14012002@gmail.com>
2022-09-09 20:25:39 +0530

    added backend frontend folder

---
+++ frontend/background.js
@@ -0,0 +1,129 @@
+// let port = 8080;
+// let baseURL = `http://amazon-pricer-tracker-server-599563671.ap-south-1.elb.amazonaws.com`;
+let port = 3000;
+// let baseURL = `http://awseb-awseb-lld2fl9uz4gd-316441135.ap-south-1.elb.amazonaws.com/v1`;
+let baseURL = `http://localhost:3000/v1`;
+
+
+chrome.runtime.onInstalled.addListener(details=>{
+    console.log(details.reason);
+    chrome.storage.sync.set({users : []});
+})
+
+let log_sign = async (request, response, url) => {
+    try {
+        const resp = await fetch(`${baseURL}/user/${url}`, {
+            method: "POST",
+            body: JSON.stringify(request),
+            headers: { "Content-type": "application/json" }
+        });
+        
+        const res = await resp.json();
+        console.log(res);
+        if(res.success === true){
+            console.log(res.data);
+            chrome.storage.sync.set({ users: [res.data] });
+            response({ user_logged_in: true });
+        }
+        else {
+            chrome.storage.sync.set({ "user_login_errors": res.data });
+            response({ user_logged_in: false });
+        }
+    }
+    catch (err) {
+        chrome.storage.sync.set({ "user_login_errors": err.message });
+        response({ user_logged_in: false });
+    }
+}
+
+let add_product = async (request,sender, response)=>{
+    let ob = {name : request.name, url : request.url, seller : request.seller, current_price : request.currPrice, threshold_price : request.threshPrice, userId : request.userid}
+    try {
+        let resp = await fetch(`${baseURL}/addproduct`, {
+            method: "POST",
+            body: JSON.stringify(ob),
+            headers: { "Content-type": "application/json" },
+        })
+        let res = await resp.json();
+        if(res.success === true){
+            response({success : true});
+        }else{
+            response({success : false});
+        }
+    }
+    catch (err) {
+        console.log(err);
+        response({success : false});
+    }
+}
+
+let remove_item = async (request, sender, response) => {
+    let obj = { product_id: request.item_id, userId : request.userId };
+    try {
+        let resp = await fetch(`${baseURL}/remove_item`, {
+            method: "POST",
+            body: JSON.stringify(obj),
+            headers: { "Content-type": "application/json" }
+        });
+
+        let res = await resp.json();
+        if(res.success === true){
+            response({ removed: true });
+        }else{
+            response({ removed: false});
+        }
+    }
+    catch (err) {
+        console.log(err);
+        response({ removed: false });
+    }
+}
+
+
+let verify_product = async (request, sender, response) => {
+    let obj = { url : request.url, userId : request.userId };
+    console.log(obj);
+    try {
+        let resp = await fetch(`${baseURL}/verifyProduct`, {
+            method: "POST",
+            body: JSON.stringify(obj),
+            headers: { "Content-type": "application/json" }
+        });
+        let res = await resp.json();
+        let data = res.data;
+        if(res.success === true){
+            response({data : data});
+        }else{
+            response({data : {available: false}});
+        }
+    }
+    catch (err) {
+        console.log(err);
+        response({data : {available: false}});
+    }
+}
+
+
+chrome.runtime.onMessage.addListener((request, sender, response) => {
+    if (request.type === "log_sign") {
+        if (request.username) {
+            log_sign(request, response, "signup")
+        }
+        else {
+            log_sign(request, response, "login");
+        }
+        return true;
+    }
+    else if (request.type === "add_product") {
+        add_product(request, sender, response);
+        return true;
+    }
+    else if (request.type === "remove_item") {
+        remove_item(request, sender, response);
+        return true;
+    }else if(request.type === "verify_product"){
+        verify_product(request, sender, response);
+        return true;
+    }
+
+})
---
+++ frontend/content.js
@@ -0,0 +1,118 @@
+// let port = 8080;
+// let baseURL = `http://amazon-pricer-tracker-server-599563671.ap-south-1.elb.amazonaws.com`;
+let port = 3000;
+let baseURL = `http://localhost:3000/v1`;
+// let baseURL = `http://localhost:3000/v1/`;
+let flipkart_price_selector = "._30jeq3";
+let amazon_price_selector = ".a-offscreen";
+
+
+let find_price = (price_str)=>{
+    if(price_str === undefined || price_str === "unavailable"){
+        return null;
+    }else{
+        return Math.floor(parseFloat(price_str.substr(1).replaceAll(",", "")));
+    }
+
+}
+
+
+let find_price_str = (seller) => {
+    let el;
+    if (seller === 'flipkart') {
+        el = document.querySelector(flipkart_price_selector);
+    } else if (seller === 'amazon') {
+        el = document.querySelector(amazon_price_selector);
+    }
+    return el.innerHTML;
+}
+
+
+
+chrome.storage.sync.get(["users", "selected_items"], (data) => {
+
+    if (data.users.length == 1) {
+
+        const product_title_el = document.querySelector("#productTitle");
+
+        let product_addition = ()=>{
+
+        let form_el = document.createElement("form");
+        form_el.classList.add("form");
+
+        let button_el = document.createElement("button");
+        button_el.appendChild(document.createTextNode("Add to track"));
+
+        const threshold_price_el = document.createElement("input");
+        threshold_price_el.type = "number";
+        threshold_price_el.appendChild(document.createTextNode(""));
+        threshold_price_el.placeholder = "Enter the threshold value";
+        threshold_price_el.required = true;
+        threshold_price_el.classList.add("thresh_val");
+
+        const br_el = document.createElement("br");
+
+        const error_el = document.createElement("div");
+
+        form_el.appendChild(threshold_price_el);
+        form_el.appendChild(error_el);
+        form_el.appendChild(button_el);
+
+        product_title_el.appendChild(br_el);
+        product_title_el.appendChild(form_el);
+
+        let ob = { type: "add_product", url: "https://www.amazon.in/Redmi-Sporty-Orange-64GB-Storage/dp/B08696W3B3/ref=sr_1_1?dchild=1&keywords=redmi+note+7&qid=1625587510&sr=8-1", name: "redmi note 7", currPrice: 11000, threshPrice: 10000, userid: data.users[0].id };
+        button_el.addEventListener("click", async (e) => {
+            e.preventDefault();
+            error_el.innerHTML = "";
+            if (threshold_price_el.value == "") {
+                error_el.innerHTML = "Please enter some value";
+                error_el.style = "color : Red; font-weight : 350";
+            }
+            else {
+                button_el.innerText = "Adding...";
+                ob.threshPrice = threshold_price_el.value.replace(",", "");
+                ob.name = product_title_el.innerText.substring(0, 15);
+                ob.name += "..."
+                ob.url = window.location.href;
+                if(ob.url.includes("https://www.amazon.in")){
+                    ob.seller = "amazon";
+                }else if(ob.url.includes("https://www.flipkart.com")){
+                    ob.seller = "flipkart";
+                }
+                let price_str = find_price_str(ob.seller);
+                ob.currPrice = find_price(price_str);
+
+                chrome.runtime.sendMessage(ob, (response) => {
+                    if (response.success) {
+                        button_el .innerHTML = "Added";
+                        button_el .disabled = true;
+                    }
+                    else {
+                        //code to handle errors
+                    }
+                })
+            }
+
+        })
+        }
+
+
+        let temp = {type : "verify_product", url : window.location.href, userId : data.users[0].id};
+
+
+        chrome.runtime.sendMessage(temp, (response) => {
+            if(response.data.available){
+                let button_el = document.createElement("button");
+                button_el.appendChild(document.createTextNode("Added"));
+                const br_el = document.createElement("br");
+                product_title_el.appendChild(br_el);
+                product_title_el.appendChild(button_el);
+                button_el.disabled = true;
+            }
+            else{
+                product_addition();
+            }
+        })
+    }
+})
